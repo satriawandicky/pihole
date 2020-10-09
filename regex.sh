01ad80046de4eec307f6027e1cb7dd0b3d534a48
Dicky Satriawan <dicky.satriawan8@gmail.com>
2020-10-09 13:12:21 +0700

    update regex issue

--- regex.sh
+++ regex.sh
@@ -3,7 +3,7 @@ PIHOLE_LOCATION="/etc/pihole"
 GRAVITY_UPDATE_COMMAND="pihole -regex"
 
 #script regex -- untuk mencegah false positif
-echo -e " \e[1m Script ini akan mendownload dan menambahkan regex dari repository dari blocklist.list \e[0m"
+echo -e " \e[1m Script ini akan mendownload dan menambahkan regex dari repository dari regex.txt \e[0m"
 echo -e "\n"
 echo -e " \e[1m ..... \e[0m"
 sleep 1
@@ -14,14 +14,14 @@ if [ "$(id -u)" != "0" ] ; then
 	exit 2
 fi
 
-curl -sS https://raw.githubusercontent.com/satriawandicky/pihole/master/regex.list | sudo tee -a "${PIHOLE_LOCATION}"/regex.list >/dev/null
+curl -sS https://raw.githubusercontent.com/satriawandicky/pihole/master/regex.txt | sudo tee -a "${PIHOLE_LOCATION}"/regex.txt >/dev/null
 echo -e " ${TICK} \e[32m Menambhakan regex ke daftar regex pihole... \e[0m"
 sleep 0.1
 echo -e " ${TICK} \e[32m menghapus kemungkinan regex yang sama... \e[0m"
-mv "${PIHOLE_LOCATION}"/regex.list "${PIHOLE_LOCATION}"/regex.list.old && cat "${PIHOLE_LOCATION}"/regex.list.old | sort | uniq >> "${PIHOLE_LOCATION}"/regex.list
+mv "${PIHOLE_LOCATION}"/regex.txt "${PIHOLE_LOCATION}"/regex.txt.old && cat "${PIHOLE_LOCATION}"/regex.txt.old | sort | uniq >> "${PIHOLE_LOCATION}"/regex.txt
 
-echo -e " [...] \e[32m Pi-hole gravity memperbarui list....harap tunggu \e[0m"
-${GRAVITY_UPDATE_COMMAND} $(cat /etc/pihole/regex.list | xargs) > /dev/null
+echo -e " [...] \e[32m Pi-hole gravity memperbarui txt....harap tunggu \e[0m"
+${GRAVITY_UPDATE_COMMAND} $(cat /etc/pihole/regex.txt | xargs) > /dev/null
  
 echo -e " ${TICK} \e[32m Pi-hole's gravity berhasil di update \e[0m"
 echo -e " ${TICK} \e[32m Selesai... \e[0m"
