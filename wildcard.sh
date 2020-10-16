6f549bb255b303083ba4dbf45b14d92c8cca6d24
Dicky Satriawan <dicky.satriawan8@gmail.com>
2020-10-16 17:54:17 +0700

    sort

--- wildcard.sh
+++ wildcard.sh
@@ -23,8 +23,8 @@ echo " ${TICK} \e[32m Selesai undo wildcard... \e[0m"
 echo " \e[1m ................... \e[0m"
 sleep 0.1
 
-curl -sS https://raw.githubusercontent.com/satriawandicky/pihole/master/wildcard.txt | sudo tee -a "${PIHOLE_LOCATION}"/wildcard.txt  > /dev/null
-sort -u "${PIHOLE_LOCATION}"/wildcard.txt > "${PIHOLE_LOCATION}"/wildcard.txt
+curl -sS https://raw.githubusercontent.com/satriawandicky/pihole/master/wildcard.txt | sort -u "${PIHOLE_LOCATION}"/wildcard.txt > /dev/null
+
 echo " ${TICK} \e[32m Menambahkan domain ke daftar wildcard pihole... \e[0m"
 sleep 0.1
 echo " ${TICK} \e[32m menghapus kemungkinan domain yang sama... \e[0m"
