024f600175aa74c7398353e119a0d531c64fb688
Dicky Satriawan <dicky.satriawan8@gmail.com>
2020-10-16 16:26:40 +0700

    xx

--- wildcard.sh
+++ wildcard.sh
@@ -24,11 +24,11 @@ echo " ${TICK} \e[32m Selesai undo wildcard... \e[0m"
 sleep 1
 
 
-curl -sS https://raw.githubusercontent.com/satriawandicky/pihole/master/wildcard.txt | sudo tee -a "${PIHOLE_LOCATION}"/wildcard.txt | sort | uniq > /dev/null
+curl -sS https://raw.githubusercontent.com/satriawandicky/pihole/master/wildcard.txt | sudo tee -a "${PIHOLE_LOCATION}"/wildcard.txt > /dev/null
 echo " ${TICK} \e[32m Menambahkan domain ke daftar wildcard pihole... \e[0m"
 sleep 0.1
 echo " ${TICK} \e[32m menghapus kemungkinan domain yang sama... \e[0m"
-
+sort -u /etc/pihole/wildcard.txt > /etc/pihole/wildcard.txt
 sleep 1
 
 echo " [...] \e[32m Pi-hole gravity memperbarui list....harap tunggu \e[0m"
@@ -36,7 +36,7 @@ ${GRAVITY_UPDATE_COMMAND} $(cat /etc/pihole/wildcard.txt | xargs) > /dev/null
 echo " ${TICK} \e[32m Pi-hole's gravity berhasil di update \e[0m"
 echo " ${TICK} \e[32m Selesai... \e[0m"
 
-cat /etc/pihole/wildcard.txt | xargs
+cat /etc/pihole/wildcard.txt
 echo " \e[1m  salam @satriawandicky \e[0m"
 echo " \e[1m  Happy AdBlocking :)\e[0m"
 echo "\n\n"
