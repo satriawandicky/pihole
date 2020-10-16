c97f61868ea2a0afff7fd1fdcd4d32093f5a8630
Dicky Satriawan <dicky.satriawan8@gmail.com>
2020-10-16 19:31:42 +0700

    akan

--- wildcard.sh
+++ wildcard.sh
@@ -7,8 +7,6 @@ echo " \e[1m Script ini akan mendownload dan menambahkan domain dari repository 
 echo "\n"
 echo " \e[1m ..... \e[0m"
 sleep 1
-echo "\n"
-
 if [ "$(id -u)" != "0" ] ; then
 	echo "script ini membutuhkan root access... akses root diperlukan telelbih dahulu!"
 	exit 2
@@ -22,9 +20,7 @@ ${GRAVITY_UNDO_WILD} $(cat /etc/pihole/undo-wildcard.txt | xargs) > /dev/null
 echo " ${TICK} \e[32m Selesai undo wildcard... \e[0m"
 echo " \e[1m ................... \e[0m"
 sleep 0.1
-
 curl -sS https://raw.githubusercontent.com/satriawandicky/pihole/master/wildcard.txt | sudo tee -a "${PIHOLE_LOCATION}"/wildcard.txt > /dev/null
-
 echo " ${TICK} \e[32m Menambahkan domain ke daftar wildcard pihole... \e[0m"
 sleep 0.1
 echo " ${TICK} \e[32m menghapus kemungkinan domain yang sama... \e[0m"
