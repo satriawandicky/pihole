84c338885037055652e6a36e6627b3538b219f6a
Dicky Satriawan <dicky.satriawan8@gmail.com>
2020-10-16 19:16:05 +0700

    sempoa

--- wildcard.sh
+++ wildcard.sh
@@ -29,17 +29,11 @@ echo " ${TICK} \e[32m Menambahkan domain ke daftar wildcard pihole... \e[0m"
 sleep 0.1
 echo " ${TICK} \e[32m menghapus kemungkinan domain yang sama... \e[0m"
 sleep 1
-
-echo " [...] \e[32m Pi-hole gravity memperbarui list....harap tunggu \e[0m"
 echo " [...] \e[32m mengupdate list berikut.... \e[0m"
-echo "\n"
-
 echo "\n"
 mv "${PIHOLE_LOCATION}"/wildcard.txt "${PIHOLE_LOCATION}"/up-wildcard.txt && cat "${PIHOLE_LOCATION}"/up-wildcard.txt | sort | uniq > "${PIHOLE_LOCATION}"/wildcard.txt
 sleep 1
-
 cat /etc/pihole/wildcard.txt
-
 ${GRAVITY_UPDATE_COMMAND} $(cat /etc/pihole/wildcard.txt | xargs) > /dev/null
 echo " ${TICK} \e[32m Pi-hole's gravity berhasil di update \e[0m"
 echo " ${TICK} \e[32m Selesai... \e[0m"
